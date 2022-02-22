4701f571dc13f9153558f943ff50d47817774014
mag1313 <maglovanyi13@gmail.com>
2022-02-22 15:56:39 -0500

    adding utilities

--- src/test/java/com/nextbasecrm/utilities/BrowserUtils.java
+++ src/test/java/com/nextbasecrm/utilities/BrowserUtils.java
@@ -3,6 +3,8 @@ package com.nextbasecrm.utilities;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 
+import java.util.Set;
+
 public class BrowserUtils {
 
     //This method accepts a String "expectedTitle" and Asserts if it is true
@@ -10,6 +12,30 @@ public class BrowserUtils {
         Assert.assertEquals(driver.getTitle(), expectedTitle);
     }
 
+    //This method will accept int and execute Thread.sleep
+    public static void sleep(int second) {
+        second *= 1000;
+        try {
+            Thread.sleep(second);
+        } catch (InterruptedException e) {
 
+        }
+    }
+
+    public static void switchWindowAndVerify(WebDriver driver, String expectedInUrl, String expectedTitle) {
+
+        Set<String> allWindowsHandles = driver.getWindowHandles();
+        for (String each : allWindowsHandles) {
+            driver.switchTo().window(each);
+            System.out.println("Current URL: " + driver.getCurrentUrl());
+
+            if (driver.getCurrentUrl().contains(expectedInUrl)) {
+                break;
+            }
+        }
+        String actualTitle = driver.getTitle();
+        Assert.assertTrue(actualTitle.contains(expectedTitle));
+
+    }
 
 }
