393cf22c589cac05c5fc212d9a8206fc1d018633
Hazrat Shah <shah_pakhtyal@live.com>
2022-02-22 21:13:14 -0500

    It's true.

--- src/test/java/com/nextbasecrm/tests/Test.java
+++ src/test/java/com/nextbasecrm/tests/Test.java
@@ -3,5 +3,7 @@ package com.nextbasecrm.tests;
 public class Test {
     public static void main(String[] args) {
 
+        //It is so confusing for now, and I am sure it will make sense in the hereafter.
+
     }
 }
