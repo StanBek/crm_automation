afc8ae234f29856bc852cc2b23b1a5f046ca27f4
Eli <eli.j.gotay@gmail.com>
2022-02-27 14:07:58 -0500

    US2 Update

--- src/test/java/com/nextbasecrm/tests/US2_RememberMe_Checkbox.java
+++ src/test/java/com/nextbasecrm/tests/US2_RememberMe_Checkbox.java
@@ -3,32 +3,51 @@ package com.nextbasecrm.tests;
 import com.nextbasecrm.utilities.BrowserUtils;
 import com.nextbasecrm.utilities.ConfigurationReader;
 import com.nextbasecrm.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
+import java.awt.*;
 import java.util.concurrent.TimeUnit;
 
 public class US2_RememberMe_Checkbox {
 
-    WebDriver driver;
+        WebDriver driver;
 
-    @BeforeMethod
-    public void setUp() {
-        driver = WebDriverFactory.getDriver(ConfigurationReader.getProperty("browser"));
-        driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-        //  1-go to login page
-        driver.get(ConfigurationReader.getProperty("env"));
+        @BeforeMethod
+        public void setUp () {
+            driver = WebDriverFactory.getDriver("chrome");
+            driver.manage().window().maximize();
+            driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+            driver.get("https://login2.nextbasecrm.com/");
+        }
+
+        @AfterMethod
+        public void tearDown () {
+            BrowserUtils.sleep(3);
+            driver.close();
+        }
+
+        @Test
+        public void verify_Checkbox() {
+            //Locate checkbox
+            WebElement verify_Checkbox = driver.findElement(By.xpath("//input[@name='USER_REMEMBER']"));
+            verify_Checkbox.isDisplayed();
+
+        }
+
+        @Test
+        public void click_Checkbox(){
+            //Click checkbox
+            WebElement click_Checkbox = driver.findElement(By.xpath("//input[@name='USER_REMEMBER']"));
+            click_Checkbox.click();
+        }
+
+
     }
-
-    @AfterMethod
-    public void tearDown() {
-        BrowserUtils.sleep(3);
-        driver.close();
-    }
-
-}
 
 //helpdesk67@cydeo.com
 //helpdesk68@cydeo.com
