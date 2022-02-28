389f9adc0be1d94db3386711f5f0cdec4ee025f0
Ramia <ramiaarnouk@yahoo.com>
2022-02-28 16:28:35 -0500

    CRM_Ramia_MoreTab

--- src/test/java/com/nextbasecrm/tests/US_006_MoreTab.java
+++ src/test/java/com/nextbasecrm/tests/US_006_MoreTab.java
@@ -16,23 +16,23 @@ public class US_006_MoreTab {
     WebDriver driver;
 
     @BeforeMethod
-    public void setUp(){
-        driver= WebDriverFactory.getDriver("chrome");
+    public void setUp() {
+        driver = WebDriverFactory.getDriver("chrome");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://login2.nextbasecrm.com/");
-        CRM_Utilities.crm_login(driver,"hr67@cydeo.com","UserUser");
     }
     @AfterMethod
     public void tearDown(){
         driver.close();
     }
     @Test
-    public void MoreDropDown_HR(){
+    public void MoreDropDown_HR1(){
         //hr67@cydeo.com
         //hr68@cydeo.com
         //hr69@cydeo.com
         //UserUser
+        CRM_Utilities.crm_login(driver,"hr67@cydeo.com","UserUser");
         WebElement moreButtonLocator=driver.findElement(By.xpath("//*[@id=\"feed-add-post-form-link-text\"]"));
         moreButtonLocator.click();
         List<WebElement> AllMore_Options=driver.findElements(By.xpath("//*[@class='menu-popup-item-text']"));
@@ -47,6 +47,7 @@ public class US_006_MoreTab {
         //    helpdesk68@cydeo.com
         //    helpdesk69@cydeo.com
         //    UserUser
+        CRM_Utilities.crm_login(driver,"helpDesk67@cydeo.com","UserUser");
         WebElement moreButtonLocator = driver.findElement(By.xpath("//*[@id=\"feed-add-post-form-link-text\"]"));
         moreButtonLocator.click();
         List<WebElement> AllMore_Options = driver.findElements(By.xpath("//*[@class='menu-popup-item-text']"));
@@ -61,6 +62,7 @@ public class US_006_MoreTab {
 //    marketing68@cydeo.com
 //    marketing69@cydeo.com
         //    UserUser
+        CRM_Utilities.crm_login(driver,"marketing67@cydeo.com","UserUser");
         WebElement moreButtonLocator = driver.findElement(By.xpath("//*[@id=\"feed-add-post-form-link-text\"]"));
         moreButtonLocator.click();
         List<WebElement> AllMore_Options = driver.findElements(By.xpath("//*[@class='menu-popup-item-text']"));
