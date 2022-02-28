6798fce5ea9bcd3d14c77911696a174effcfcac0
Aygun Jarullah <aygunxanim@gmail.com>
2022-02-27 19:22:42 -0500

    everything looks good

--- src/test/java/com/nextbasecrm/tests/utilities/WebDriverFactory.java
+++ src/test/java/com/nextbasecrm/tests/utilities/WebDriverFactory.java
@@ -22,3 +22,13 @@ public class WebDriverFactory {
     }
 
 }
+
+// TASK: NEW METHOD CREATION
+// Method name : getDriver
+// Static method
+// Accepts String arg: browserType
+//   - This arg will determine what type of browser is opened
+//   - if "chrome" passed --> it will open chrome browser
+//   - if "firefox" passed --> it will open firefox browser
+// RETURN TYPE: "WebDriver"
+
