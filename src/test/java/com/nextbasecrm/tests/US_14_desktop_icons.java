9eaccf950c647a98aa0549b60d9d43e02bd3a713
Asel Akylbekova <amel_88@mail.ru>
2022-02-28 14:15:45 -0700

    Test done

--- src/test/java/com/nextbasecrm/tests/US_14_desktop_icons.java
+++ src/test/java/com/nextbasecrm/tests/US_14_desktop_icons.java
@@ -14,8 +14,8 @@ import java.util.concurrent.TimeUnit;
 
 public class US_14_desktop_icons {
     public WebDriver driver;
-    String userName = "helpdesk67@cybertekschool.com";
-    String password = "UserUser";
+    String userName;
+    String password;
 
 
     @BeforeMethod
@@ -24,43 +24,117 @@ public class US_14_desktop_icons {
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
         driver.get("https://login2.nextbasecrm.com");
-        CRM_Utilities.crm_login(driver,userName,password);
+
 
     }
+
     @AfterMethod
-    public void tearMethod(){
+    public void tearMethod() {
         driver.close();
     }
 
     @Test
-    public void crm_desktop_icons() throws InterruptedException {
+    public void crm_desktop_icons_helpDesk() throws InterruptedException {
+        userName = "helpdesk67@cydeo.com";
+        password = "UserUser";
+        CRM_Utilities.crm_login(driver, userName, password);
 
-        WebElement desktopIcon =driver.findElement(By.xpath("(//div[@class='b24-app-block b24-app-desktop']/div)[1]"));
+        WebElement desktopIcon = driver.findElement(By.xpath("(//div[@class='b24-app-block b24-app-desktop']/div)[1]"));
 
 
         String actualIcon = desktopIcon.getText();
         String expectedIcon = "DESKTOP CLIENT";
 
-        Assert.assertEquals(actualIcon,expectedIcon);
+        Assert.assertEquals(actualIcon, expectedIcon);
 
         WebElement macOsBtn = driver.findElement(By.xpath("//*[contains(text(),'Mac OS')]"));
         String actualMacOsBtn = macOsBtn.getText();
         String expectedMacOsBtn = "MAC OS";
 
-        Assert.assertEquals(actualMacOsBtn,expectedMacOsBtn);
+        Assert.assertEquals(actualMacOsBtn, expectedMacOsBtn);
 
         WebElement windowsBtn = driver.findElement(By.xpath("//*[@class='b24-app-block b24-app-desktop']/div/a/span[contains(text(),'Windows')]"));
         String actualWindowsBtn = windowsBtn.getText();
         String expectedWindowsBtn = "WINDOWS";
 
-        Assert.assertEquals(actualWindowsBtn,expectedWindowsBtn);
+        Assert.assertEquals(actualWindowsBtn, expectedWindowsBtn);
 
         WebElement linuxBtn = driver.findElement(By.xpath("//*[@class='b24-app-block b24-app-desktop']/div/a/span[contains(text(),'Linux')]"));
         String actualLinuxBtn = linuxBtn.getText();
         String expectedLinuxBtn = "LINUX";
 
-        Assert.assertEquals(actualLinuxBtn,expectedLinuxBtn);
+        Assert.assertEquals(actualLinuxBtn, expectedLinuxBtn);
 
+
+    }
+
+    @Test
+    public void crm_desktop_icons_marketing() throws InterruptedException {
+        userName = "hr67@cydeo.com";
+        password = "UserUser";
+        CRM_Utilities.crm_login(driver, userName, password);
+
+        WebElement desktopIcon = driver.findElement(By.xpath("(//div[@class='b24-app-block b24-app-desktop']/div)[1]"));
+
+
+        String actualIcon = desktopIcon.getText();
+        String expectedIcon = "DESKTOP CLIENT";
+
+        Assert.assertEquals(actualIcon, expectedIcon);
+
+        WebElement macOsBtn = driver.findElement(By.xpath("//*[contains(text(),'Mac OS')]"));
+        String actualMacOsBtn = macOsBtn.getText();
+        String expectedMacOsBtn = "MAC OS";
+
+        Assert.assertEquals(actualMacOsBtn, expectedMacOsBtn);
+
+        WebElement windowsBtn = driver.findElement(By.xpath("//*[@class='b24-app-block b24-app-desktop']/div/a/span[contains(text(),'Windows')]"));
+        String actualWindowsBtn = windowsBtn.getText();
+        String expectedWindowsBtn = "WINDOWS";
+
+        Assert.assertEquals(actualWindowsBtn, expectedWindowsBtn);
+
+        WebElement linuxBtn = driver.findElement(By.xpath("//*[@class='b24-app-block b24-app-desktop']/div/a/span[contains(text(),'Linux')]"));
+        String actualLinuxBtn = linuxBtn.getText();
+        String expectedLinuxBtn = "LINUX";
+
+        Assert.assertEquals(actualLinuxBtn, expectedLinuxBtn);
+
+
+    }
+
+    @Test
+    public void crm_desktop_icons_hr() throws InterruptedException {
+        userName = "marketing67@cydeo.com";
+        password = "UserUser";
+        CRM_Utilities.crm_login(driver, userName, password);
+
+
+        WebElement desktopIcon = driver.findElement(By.xpath("(//div[@class='b24-app-block b24-app-desktop']/div)[1]"));
+
+
+        String actualIcon = desktopIcon.getText();
+        String expectedIcon = "DESKTOP CLIENT";
+
+        Assert.assertEquals(actualIcon, expectedIcon);
+
+        WebElement macOsBtn = driver.findElement(By.xpath("//*[contains(text(),'Mac OS')]"));
+        String actualMacOsBtn = macOsBtn.getText();
+        String expectedMacOsBtn = "MAC OS";
+
+        Assert.assertEquals(actualMacOsBtn, expectedMacOsBtn);
+
+        WebElement windowsBtn = driver.findElement(By.xpath("//*[@class='b24-app-block b24-app-desktop']/div/a/span[contains(text(),'Windows')]"));
+        String actualWindowsBtn = windowsBtn.getText();
+        String expectedWindowsBtn = "WINDOWS";
+
+        Assert.assertEquals(actualWindowsBtn, expectedWindowsBtn);
+
+        WebElement linuxBtn = driver.findElement(By.xpath("//*[@class='b24-app-block b24-app-desktop']/div/a/span[contains(text(),'Linux')]"));
+        String actualLinuxBtn = linuxBtn.getText();
+        String expectedLinuxBtn = "LINUX";
+
+        Assert.assertEquals(actualLinuxBtn, expectedLinuxBtn);
 
 
     }
