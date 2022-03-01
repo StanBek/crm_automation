47fdde42e83a6679cb2e987143e5cc4608f04f9f
Aygun Jarullah <aygunxanim@gmail.com>
2022-02-28 19:21:11 -0500

    User story 5 is done

--- src/test/java/com/nextbasecrm/tests/US1_Login_to_HomePage.java
+++ src/test/java/com/nextbasecrm/tests/US1_Login_to_HomePage.java
@@ -1,6 +1,6 @@
 package com.nextbasecrm.tests;
 
-import com.nextbasecrm.utilities.WebDriverFactory;
+import com.nextbasecrm.tests.utilities.WebDriverFactory;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
