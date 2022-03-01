ceda38e741a5fc4c40f7b9831227be2431ad21eb
cristinamartinescu <martikristi92@yahoo.com>
2022-02-28 22:02:06 -0500

    US10 finished

--- src/test/java/com/nextbasecrm/tests/US10_CreateATask.java
+++ src/test/java/com/nextbasecrm/tests/US10_CreateATask.java
@@ -15,7 +15,19 @@ import java.util.concurrent.TimeUnit;
 
 public class US10_CreateATask {
 
-    public WebDriver driver;
+    WebDriver driver;
+    WebElement username;
+    WebElement password;
+    WebElement clickLoginButton;
+    WebElement taskTab;
+    WebElement inputTitle;
+    WebElement contentMessage;
+    WebElement sendBtn;
+    WebElement verify;
+
+
+
+
 
     @BeforeMethod
     public void setupMethod() {
@@ -25,53 +37,163 @@ public class US10_CreateATask {
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
         driver.get(" https://login2.nextbasecrm.com/");
-        WebElement inputUsername = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[1]/input"));
-        inputUsername.sendKeys("hr68@cydeo.com");
 
-        WebElement inputPassword = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[2]/input"));
-        inputPassword.sendKeys("UserUser");
-
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[2]/input"));
-        loginButton.click();
     }
 
 
     //            Users click the TASK tab
     @Test
-    public void test_create() {
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
+    public void test_create_as_user_hr68() {
+
+        username = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[1]/input"));
+        username.sendKeys("hr68@cydeo.com");
+
+        password= driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[2]/input"));
+        password.sendKeys("UserUser");
+
+        clickLoginButton = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[2]/input"));
+        clickLoginButton.click();
+
+
+
+       taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
         taskTab.click();
         Assert.assertTrue(taskTab.isDisplayed());
 
-        WebElement inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
+        inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
         inputTitle.sendKeys("CYDEO");
 
-
+//    Users click the SEND button
         driver.switchTo().frame(driver.findElement(By.xpath("//*[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
-        WebElement contentMessage = driver.findElement(By.cssSelector("body[style=\"min-height: 84px;\"]"));
+      contentMessage = driver.findElement(By.cssSelector("body[style=\"min-height: 84px;\"]"));
         contentMessage.sendKeys("Hello World");
         driver.switchTo().parentFrame();
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
+      sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
         sendBtn.click();
-
-        WebElement verify = driver.findElement(By.xpath("//div[@id=\"sonet_log_day_item_KB7VoSvE\"]"));
+//            Verify the task is displayed on the feed
+      verify = driver.findElement(By.xpath("//div[@id=\"sonet_log_day_item_KB7VoSvE\"]"));
         verify.isDisplayed();
 
     }
 
     @Test
-    public void errorMsg() {
-        WebElement taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
+    public void errorMsg_as_user_hr68() {
+        //            Users write task title and task content/message
+         taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
         taskTab.click();
         Assert.assertTrue(taskTab.isDisplayed());
 
-        WebElement inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
+        inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
         inputTitle.sendKeys("");
 
-        WebElement sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
+        sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
         sendBtn.click();
 
-        WebElement verify = driver.findElement(By.xpath("//div[@id=\"feed-add-post-content-tasks-container\"]"));
+        verify = driver.findElement(By.xpath("//div[@id=\"feed-add-post-content-tasks-container\"]"));
+        verify.isDisplayed();
+    }
+
+    @Test
+    public void test_create_as_user_helpdesk68() {
+
+        username = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[1]/input"));
+        username.sendKeys("helpdesk68@cydeo.com");
+
+        password= driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[2]/input"));
+        password.sendKeys("UserUser");
+
+        clickLoginButton = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[2]/input"));
+        clickLoginButton.click();
+
+
+
+        taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
+        taskTab.click();
+        Assert.assertTrue(taskTab.isDisplayed());
+
+        inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
+        inputTitle.sendKeys("CYDEO");
+
+//    Users click the SEND button
+        driver.switchTo().frame(driver.findElement(By.xpath("//*[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
+        contentMessage = driver.findElement(By.cssSelector("body[style=\"min-height: 84px;\"]"));
+        contentMessage.sendKeys("Hello World");
+        driver.switchTo().parentFrame();
+        sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
+        sendBtn.click();
+//            Verify the task is displayed on the feed
+        verify = driver.findElement(By.xpath("//div[@id=\"sonet_log_day_item_KB7VoSvE\"]"));
+        verify.isDisplayed();
+
+    }
+
+    @Test
+    public void errorMsg_as_user_helpdesk68() {
+        //            Users write task title and task content/message
+        taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
+        taskTab.click();
+        Assert.assertTrue(taskTab.isDisplayed());
+
+        inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
+        inputTitle.sendKeys("");
+
+        sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
+        sendBtn.click();
+
+        verify = driver.findElement(By.xpath("//div[@id=\"feed-add-post-content-tasks-container\"]"));
+        verify.isDisplayed();
+    }
+
+
+
+    @Test
+    public void test_create_as_user_marketing68() {
+
+        username = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[1]/input"));
+        username.sendKeys("marketing68@cydeo.com");
+
+        password= driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[1]/div[2]/input"));
+        password.sendKeys("UserUser");
+
+        clickLoginButton = driver.findElement(By.xpath("//*[@id=\"login-popup\"]/form/div[2]/input"));
+        clickLoginButton.click();
+
+
+
+        taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
+        taskTab.click();
+        Assert.assertTrue(taskTab.isDisplayed());
+
+        inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
+        inputTitle.sendKeys("CYDEO");
+
+//    Users click the SEND button
+        driver.switchTo().frame(driver.findElement(By.xpath("//*[@id=\"bx-html-editor-iframe-cnt-lifefeed_task_form\"]/iframe")));
+        contentMessage = driver.findElement(By.cssSelector("body[style=\"min-height: 84px;\"]"));
+        contentMessage.sendKeys("Hello World");
+        driver.switchTo().parentFrame();
+        sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
+        sendBtn.click();
+//            Verify the task is displayed on the feed
+        verify = driver.findElement(By.xpath("//div[@id=\"sonet_log_day_item_KB7VoSvE\"]"));
+        verify.isDisplayed();
+
+    }
+
+    @Test
+    public void errorMsg_as_user_marketing68() {
+        //            Users write task title and task content/message
+        taskTab = driver.findElement(By.xpath("//span[@id='feed-add-post-form-tab-tasks']"));
+        taskTab.click();
+        Assert.assertTrue(taskTab.isDisplayed());
+
+        inputTitle = driver.findElement(By.xpath("//input[@name='ACTION[0][ARGUMENTS][data][TITLE]']"));
+        inputTitle.sendKeys("");
+
+        sendBtn = driver.findElement(By.xpath("//button[@id=\"blog-submit-button-save\"]"));
+        sendBtn.click();
+
+        verify = driver.findElement(By.xpath("//div[@id=\"feed-add-post-content-tasks-container\"]"));
         verify.isDisplayed();
     }
 
@@ -87,8 +209,8 @@ public class US10_CreateATask {
 
 
 
-//            Users write task title and task content/message
-//    Users click the SEND button
-//            Verify the task is displayed on the feed
+
+
+
 
 
