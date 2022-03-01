40c6ba73f7c88231248dd7d021f37e2875650c85
Hazrat Shah <shah_pakhtyal@live.com>
2022-02-28 19:16:51 -0500

    Yes

--- src/test/java/com/nextbasecrm/tests/Test.java
+++ src/test/java/com/nextbasecrm/tests/Test.java
@@ -4,6 +4,6 @@ public class Test {
     public static void main(String[] args) {
 
         //It is so confusing for now, and I am sure it will make sense in the hereafter.
-
+// Great team
     }
 }
