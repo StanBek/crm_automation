8f98ee792d5ad7a2e973fb72dafe81cc674ff610
Aygun Jarullah <aygunxanim@gmail.com>
2022-02-28 19:06:57 -0500

    User story 5 is done

--- src/test/java/com/nextbasecrm/tests/US05_UserSendSimpleMessage.java
+++ src/test/java/com/nextbasecrm/tests/US05_UserSendSimpleMessage.java
@@ -9,7 +9,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
-public class US_05 {
+public class US05_UserSendSimpleMessage {
 
 
     WebDriver driver;
